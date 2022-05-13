60a2ad8244693740054ce39f6cc86745825285c0
Ridwan <ridwanraji4@gmail.com>
2022-05-13 09:09:52 +0100

    deploy to heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,21 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/lyft-interview-test');
+
+// connecting to mongoDB Atlas Cluster
+const url = process.env.DATABASEURL || 'mongodb://localhost:27017/lyft-interview-test';
+
+const connectionParams={
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 module.exports = {mongoose}
